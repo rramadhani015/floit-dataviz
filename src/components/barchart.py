a23e41f3fdf27bafb44685fb7c6d1fead2d6cb47
rramadhani015 <47129927+rramadhani015@users.noreply.github.com>
2022-12-04 15:17:24 +0800

    show and filter custom data

--- src/components/barchart.py
+++ src/components/barchart.py
@@ -14,27 +14,34 @@ def render(app: Dash, data: pd.DataFrame) -> html.Div:
     )
     def update_barchart(category:list[str]) -> html.Div:
         filtered_data = data.query("category in @category")
+
+        # dfs=data.groupby('category').sum()
+        dfmean=filtered_data.groupby('category').mean()
+
         if filtered_data.shape[0] == 0:
             return html.Div("No data selected.")
         
         def create_pivot_table() -> pd.DataFrame:
             pt = filtered_data.pivot_table(
-                values=DataSchema.AVERAGE_RATING,
-                columns=[DataSchema.CATEGORY],
-                index=[DataSchema.SKU],
+                # values=dfmean.index,
+                columns=dfmean['average_rating'],
                 aggfunc="mean",
                 fill_value=0
             )
-            return pt.reset_index().sort_values(DataSchema.SKU, ascending=False)
+            return pt.reset_index()
 
         fig = px.bar(
             create_pivot_table(),
-            # x=DataSchema.CATEGORY,
-            x='Shoes',
-            y=DataSchema.SKU,
-            # color=DataSchema.CATEGORY,
-            color='Shoes',
-            text="Shoes"
+            x=dfmean.index,
+            y=dfmean['average_rating'],
+            color=dfmean.index,
+            text=dfmean['average_rating'],
+            labels={
+                     "x": "Category",
+                     "y": "Average Rating",
+                     "color":"Legends"
+                 },
+            title="Average Rating"
         )
         return html.Div(dcc.Graph(figure=fig), id=ids.BAR_CHART)
         # fig = px.bar(filtered_data, x="medal", y="count", color="nation", text="nation")
